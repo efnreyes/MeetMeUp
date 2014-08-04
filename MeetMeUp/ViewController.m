05f5ce99417f61960f5187ad8e522d0ea3c1b438
Efrén Reyes <efren.reyes@globant.com>
2014-08-04 16:46:32 -0500

    Step 1 finished

--- MeetMeUp/ViewController.m
+++ MeetMeUp/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property NSArray *results;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation ViewController
@@ -17,13 +18,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	NSURL *url = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=477d1928246a4e162252547b766d3c6d"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+
+        NSDictionary *resp = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        self.results = resp[@"results"];
+        [self.tableView reloadData];
+    }];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *meet = [self.results objectAtIndex:indexPath.row];
+    NSDictionary *venue = meet[@"venue"];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCellID"];
+    cell.textLabel.text = [meet objectForKey:@"name"];
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.results.count;
 }
 
 @end
